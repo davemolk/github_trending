8a141758887edfb4db05244e6a8e46c9fae011ea
davemolk <davemolk@gmail.com>
2022-04-06 06:36:31 -0600

    refactoring

--- gitty.py
+++ gitty.py
@@ -1,108 +1,79 @@
 from datetime import datetime
+import logging
 from time import sleep
 from urllib.parse import urljoin
+from urllib.request import Request
 import requests
 from bs4 import BeautifulSoup
 
+logging.basicConfig(filename='gitty.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 language_count = {}
-base_url = 'https://github.com/'
-
-def get_main_page():
-    headers = {
+headers = {
         'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
     }
+
+def get_main():
+    """
+    Requests github.com/trending and calls the parse
+    function on each repo found. Returns the repos object
+    """
+    base_url = 'https://github.com/'
     url = 'https://github.com/trending/'
+    repos = {}
+
     try:
         r = requests.get(url, headers=headers)
-    except requests.exceptions.RequestException:
-            return None
-    return BeautifulSoup(r.text, 'html.parser')
+    except requests.exceptions.RequestException as e:
+        logging.warning(f'Unable to get github.com/trending: {e}')
+        return None
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-def get_links():
-    soup = get_main_page()
-    repo_list = []
     for repo in soup.find_all('article', class_='Box-row'):
         repo_url = urljoin(base_url, repo.h1.a['href'])
-        repo_list.append(parse_repo(repo_url))
-    
-    return repo_list, language_count
+        logging.info(f"found these links: {repo_url}")
+        item = parse(repo_url)
+        repos[f"{item['name']}"] = item
 
-# def safe_get(soup, selector):
-#     try:
-#         attribute = soup.select(selector)
-#     except AttributeError:
-#         attribute = "Attribute missing"
-#     return attribute
+    repos['language_count'] = language_count
 
-def parse_repo(url):
-    headers = {
-        'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
-    }
+    logging.info(f"returning {len(repos)} repos...now!")
+
+    return repos
+
+def safe_get(soup, selector, attr):
+    """
+    helper function: pass in a selector string, get an attribute
+    """
+    try:
+        attribute = soup.select(f'{selector}')[0].text
+    except AttributeError:
+        logging.warning(f'unable to find <{selector}> for {attr} attribute')
+        return "Attribute missing"
+    else:
+        return attribute
+
+def parse(url):
+    """
+    Parse a given repo for desired information
+    """
     try:
         r = requests.get(url, headers=headers)
     except requests.exceptions.RequestException:
             return None
     soup = BeautifulSoup(r.text, 'html.parser')
+
+    name = safe_get(soup, 'strong a', 'name').replace('-', ' ')
+    description = safe_get(soup, 'p.f4', 'description').strip()
+    language = safe_get(soup, 'li.d-inline a span', 'language')
+    total_stars = safe_get(soup, '#repo-stars-counter-star', 'total_stars')
+    issues = safe_get(soup, '#issues-repo-tab-count', 'issues')
+    pr = safe_get(soup, '#pull-requests-repo-tab-count', 'pr')
+    url = url
+    date = str(datetime.now())
+
+    contributors = []
     
-    sleep(1)
     
-    try:
-        name = soup.strong.a.text
-    except AttributeError:
-        name = "Attribute missing"
-    try:
-        url = urljoin(base_url, soup.strong.a['href'])
-    except AttributeError:
-        url = "Attribute missing"
-    try:
-        description = soup.find('p', class_='f4').text.strip()
-    except AttributeError:
-        description = "Attribute missing"
-    try:
-        language = soup.select_one('li.d-inline a span').text
-    except AttributeError:
-        language = "Attribute missing"
-    try:
-        total_stars = soup.find(id='repo-stars-counter-star').text
-    except AttributeError:
-        total_stars = "Attribute missing"
-    try:
-        issues = soup.find(id='issues-repo-tab-count').text
-    except AttributeError:
-        issues = "Attribute missing"
-    try:
-        pr = soup.find(id='pull-requests-repo-tab-count').text
-    except AttributeError:
-        pr = "Attribute missing"
 
-    coders = soup.select('div.Layout-sidebar ul.list-style-none li.mb-2 a')
-    if coders != []:
-        contributors = []
-        for a in coders: 
-            if a['href'] != "":
-                contributors.append(a['href'])
-            else:
-                contributors.append(url)
-    else:
-        contributors = [url]
-
-    if language != "Attribute missing":
-        language_count[language] = 1 + language_count.get(language, 0)
-
-    item = {
-        'name': name,
-        'url': url,
-        'description': description,
-        "language": language,
-        "total_stars": total_stars,
-        "issues": issues,
-        "pr": pr,
-        "contributors": contributors,
-        "date": datetime.now(),
-    }
-
-    return item
-
-
-print(get_links())
--- scrap.py
+++ scrap.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from time import sleep
 from urllib.parse import urljoin
 from parso import parse
 import requests
@@ -8,9 +7,15 @@ from bs4 import BeautifulSoup
 url = 'https://github.com/kestra-io/kestra'
 base_url = 'https://github.com/'
 
+def safe_get(soup, selector, attr):
+    try:
+        attribute = soup.select(f'{selector}')[0].text
+    except AttributeError:
+        return "Attribute missing"
+    else:
+        return attribute
 
-
-def parse_repo(url):
+def parse(url):
     headers = {
         'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
     }
@@ -19,64 +24,75 @@ def parse_repo(url):
     except requests.exceptions.RequestException:
             return None
     soup = BeautifulSoup(r.text, 'html.parser')
-    
-    sleep(1)
-    
-    try:
-        name = soup.strong.a.text
-    except AttributeError:
-        name = "Attribute missing"
-    try:
-        url = urljoin(base_url, soup.strong.a['href'])
-    except AttributeError:
-        url = "Attribute missing"
-    try:
-        description = soup.find('p', class_='f4').text.strip()
-    except AttributeError:
-        description = "Attribute missing"
-    try:
-        language = soup.select_one('li.d-inline a span').text
-    except AttributeError:
-        language = "Attribute missing"
-    try:
-        total_stars = soup.find(id='repo-stars-counter-star').text
-    except AttributeError:
-        total_stars = "Attribute missing"
-    try:
-        issues = soup.find(id='issues-repo-tab-count').text
-    except AttributeError:
-        issues = "Attribute missing"
-    try:
-        pr = soup.find(id='pull-requests-repo-tab-count').text
-    except AttributeError:
-        pr = "Attribute missing"
 
-    coders = soup.select('div.Layout-sidebar ul.list-style-none li.mb-2 a')
-    print('coders', coders)
-    if coders == []:
-        contributors = []
-        for a in coders: 
-            print(type(a['href']))
-            print(a['href'])
-            if a['href'] is not None:
-                contributors.append(a['href'])
-            else:
-                contributors.append(url)
-    else:
-        contributors = [url]
+    name = safe_get(soup, 'strong a', 'name').replace('-', ' ')
+    description = safe_get(soup, 'p.f4', 'description').strip()
+    language = safe_get(soup, 'li.d-inline a span', 'language')
+    total_stars = safe_get(soup, '#repo-stars-counter-star', 'total_stars')
+    issues = safe_get(soup, '#issues-repo-tab-count', 'issues')
+    pr = safe_get(soup, '#pull-requests-repo-tab-count', 'pr')
 
-    item = {
-        'name': name,
-        'url': url,
-        'description': description,
-        "language": language,
-        "total_stars": total_stars,
-        "issues": issues,
-        "pr": pr,
-        "contributors": contributors,
-        "date": datetime.now(),
-    }
+    print(name)
+    print(description)
+    print(language)
+    print(total_stars)
+    print(issues)
+    print(pr)
+    # try:
+    #     name = soup.strong.a.text
+    # except AttributeError:
+    #     name = "Attribute missing"
+    # try:
+    #     url = urljoin(base_url, soup.strong.a['href'])
+    # except AttributeError:
+    #     url = "Attribute missing"
+    # try:
+    #     description = soup.find('p', class_='f4').text.strip()
+    # except AttributeError:
+    #     description = "Attribute missing"
+    # try:
+    #     language = soup.select_one('li.d-inline a span').text
+    # except AttributeError:
+    #     language = "Attribute missing"
+    # try:
+    #     total_stars = soup.find(id='repo-stars-counter-star').text
+    # except AttributeError:
+    #     total_stars = "Attribute missing"
+    # try:
+    #     issues = soup.find(id='issues-repo-tab-count').text
+    # except AttributeError:
+    #     issues = "Attribute missing"
+    # try:
+    #     pr = soup.find(id='pull-requests-repo-tab-count').text
+    # except AttributeError:
+    #     pr = "Attribute missing"
 
-    # print(item)
+    # coders = soup.select('div.Layout-sidebar ul.list-style-none li.mb-2 a')
+    # # print('coders', coders)
+    # if coders != []:
+    #     contributors = []
+    #     for a in coders: 
+    #         # print("type***************************", type(a['href']))
+    #         # print(a['href'])
+    #         if a['href'] != "":
+    #             contributors.append(a['href'])
+    #         else:
+    #             contributors.append(url)
+    # else:
+    #     contributors = [url]
 
-parse_repo(url)
+    # item = {
+    #     'name': name,
+    #     'url': url,
+    #     'description': description,
+    #     "language": language,
+    #     "total_stars": total_stars,
+    #     "issues": issues,
+    #     "pr": pr,
+    #     "contributors": contributors,
+    #     "date": datetime.now(),
+    # }
+
+    # return item
+
+parse(url)
